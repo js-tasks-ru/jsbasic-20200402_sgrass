960ec163be30280aa7f192472b8502a2c69bffe4
SergeyGrass <sergeygrass@gmail.com>
2020-05-05 17:49:23 +0300

    Задачи по модулю 4 решены.

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -3,4 +3,27 @@
  * @param {Element} table
  */
 function highlight(table) {
+  let rows = table.tBodies[0].rows;
+
+  for (let row of rows) {
+    // Check availability
+    let availability = row.cells[3].getAttribute('data-available');
+
+    if (availability === null) {
+      row.setAttribute('hidden', 'hidden');
+    }
+    else {
+      row.classList.add( {'true': 'available', 'false': 'unavailable' }[availability] );
+    }
+
+    // Check gender
+    let gender = {'m':'male', 'f':'female'}[row.cells[2].textContent];
+    row.classList.add(gender);
+
+    // Check underage
+    let age = +row.cells[1].textContent;
+    if (age < 18) {
+      row.style.textDecoration = 'line-through';
+    }
+  }
 }
